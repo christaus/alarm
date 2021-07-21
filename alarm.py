37c50cc2abdf264454d5789cf2c366393fc0a144
Gitea <gitea@fake.local>
2021-07-21 17:34:27 +0200

    Remove import * to a better pep8 compliant import.

--- alarm.py
+++ alarm.py
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 from datetime import datetime, timedelta
-from tkinter import *
+from tkinter import Tk, Label, Button, PhotoImage
 import pygame
 
 
